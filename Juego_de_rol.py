c60e07a2a4133b7cfc0e56b01739ac8c56c1e4c9
Bonilla23 <ikerfernandezbonilla@gmail.com>
2025-01-28 09:28:12 +0100

    Create a screen and text

--- Juego_de_rol.py
+++ Juego_de_rol.py
@@ -1,4 +1,18 @@
-# Importar las bibliotecas necesarias
+# Added Libraries [ turtle, time, random ]
 import turtle;
 import time;
 import random;
+
+# Create a variable "wn" 
+# For the Screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("RPG Game")
+
+# Create variable "texto"
+# Use it as text
+texto = turtle.Turtle()
+texto.color("white")
+texto.hideturtle()
+texto.penup()
+texto.speed(0)
