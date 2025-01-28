1efb6a054a11590eb232d15202acac3b169d5af7
Bonilla23 <ikerfernandezbonilla@gmail.com>
2025-01-28 09:37:34 +0100

    Function to select a class

--- Juego_de_rol.py
+++ Juego_de_rol.py
@@ -24,3 +24,30 @@ vidatotal = 0
 daño = 0
 agilidad = 0
 inteligencia = 0
+
+# function to select a class
+def seleccionar_clase():
+    global vida,vidatotal,daño,agilidad,inteligencia
+    
+    clases = {"Mago": (1, 10, 1, 10, 10, 20),
+              "Guerrero": (10, 20, 10, 20, 1, 10),
+              "Pícaro": (5, 15, 5, 15, 5, 15)}
+    
+    # To select a class
+    while True:
+        clase = wn.textinput("Seleccionar Clase", "Elige una clase: Mago, Guerrero o Pícaro").capitalize()
+        if clase in clases:
+            rangos = clases[clase]
+            # Random number
+            vida = random.randint(rangos[0], rangos[1])
+            daño = random.randint(rangos[2], rangos[3])
+            agilidad = random.randint(rangos[4], rangos[5])
+            inteligencia = random.randint(rangos[4], rangos[5])
+            vidatotal = vida  # To safe the max heal
+            return clase
+        else:
+            # If not select a class
+            texto.clear()
+            texto.write("Clase no válida. Intenta de nuevo.", align="center", font=("Courier", 24, "normal"))
+            time.sleep(2)
+            texto.clear()
